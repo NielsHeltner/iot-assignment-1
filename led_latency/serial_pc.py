214b0696db9be392b7915ed91bf7852a830f0e3d
Niels Heltner <niels.heltner@hotmail.com>
2019-02-21 20:49:11 +0100

    added function to write data to file

--- led_latency/serial_pc.py
+++ led_latency/serial_pc.py
@@ -1,12 +1,12 @@
 import serial
 import time
-from datetime import datetime
 
+file_location = 'data.txt'
 
 def write_to_device1():
 	with serial.Serial("COM3", 115200, timeout=None) as ser:
-		start = datetime.now()
-		print('Writing to device 1 (time: ' + str(start) + ')')
+		start = time.time()
+		print('Writing to device 1 (time: ' + start + ')')
 		ser.write('on\n'.encode())
 		
 		return start
@@ -17,17 +17,28 @@ def write_to_device1():
 def read_from_device2(start):
 	with serial.Serial("port of second device", 115200, timeout=None) as ser:
 		print('Waiting for device 2')
-		line = ser.readline().decode()
-		#print(line)
-		end = datetime.now()
+		light = ser.readline().decode().rstrip('\r\n')
+		end = time.time()
+		print(light)
 		print('Device 2 response at time: ' + end)
-		difference = end - start
-		print('Latency: ' + difference)
+
+		return end, light
+
+
+def log_to_file(*args):
+	text = '\t'.join(map(str, args))
+	with open(file_location, 'w+') as data_file:
+		data_file.write(text)
 
 
 def main():
 	start = write_to_device1()
-	read_from_device2(start)
+	end, light = read_from_device2(start)
+
+	difference = abs(start - end)
+
+	log_to_file(start, end, difference, light)
+
 
 if __name__ == '__main__':
 	main()
